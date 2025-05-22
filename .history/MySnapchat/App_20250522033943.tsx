e126441e2bd5b60de02384e06d1aa8123893f432
Safiya Jazouli <safiya.jazouli@epitech.eu>
2025-05-22 03:55:18 +0200

    splashscreen and routes

---
+++ .history/MySnapchat/App_20250522033943.tsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './Login';
+import RegisterScreen from './Register';
+import HomeScreen from './Home';
+import { AuthProvider, AuthContext } from './Auth';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  const { userToken } = useContext(AuthContext);
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Splash" component={SplashScreen} /> {/* 👈 seulement ici */}
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AuthProvider>
+  );
+};
+
+export default App;
---
+++ .history/MySnapchat/App_20250522034031.tsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './Login';
+import RegisterScreen from './Register';
+import HomeScreen from './Home';
+import { AuthProvider, AuthContext } from './Auth';
+import SplashScreen from './SplashScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  const { userToken } = useContext(AuthContext);
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Splash" component={SplashScreen} /> 
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AuthProvider>
+  );
+};
+
+export default App;
