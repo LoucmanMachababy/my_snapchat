e06c7302def0a7b35849bba2be776cc92356e950
loucman <loucman.machababy@epitech.eu>
2025-05-22 15:30:25 +0200

    WIP: modifs non terminées

---
+++ .history/MySnapchat/SplashScreen_20250522100415.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const SplashScreen = () => {
+  const navigation = useNavigation<any>();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Home'); 
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../MySnapchat/assets/snapchat.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFC00', 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 180,
+    height: 180,
+  },
+});
+
+export default SplashScreen;
